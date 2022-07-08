d52097761f4b41c51ed1e3672f1694e19aaa3df9
SadFox16 <oleg.s.kerimov@yandex.com>
2022-07-08 16:20:20 +0300

    Queue
    Made queue with array

--- elevator/src/App.vue
+++ elevator/src/App.vue
@@ -1,11 +1,11 @@
 <template>
   
   <table>
-    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call5" class="call" @click="move_Elevator5()" value="5" ref="button1">5</button> </td></tr>
-    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call4" class="call" @click="move_Elevator4()" value="4" ref="button2">4</button> </td></tr>
-    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call3" class="call" @click="move_Elevator3()" value="3" ref="button3">3</button> </td></tr>
-    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call2" class="call" @click="move_Elevator2()" value="2" ref="button4">2</button> </td></tr>
-    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call1" class="call" @click="move_Elevator1()" value="1" ref="button5">1</button> </td></tr>
+    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call5" class="call" @click="call_Elevator(5)" value="5" ref="button1">5</button> </td></tr>
+    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call4" class="call" @click="call_Elevator(4)" value="4" ref="button2">4</button> </td></tr>
+    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call3" class="call" @click="call_Elevator(3)" value="3" ref="button3">3</button> </td></tr>
+    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call2" class="call" @click="call_Elevator(2)" value="2" ref="button4">2</button> </td></tr>
+    <tr><td class="elevator_sh"> </td><td class="lvl"> <button id="call1" class="call" @click="call_Elevator(1)" value="1" ref="button5">1</button> </td></tr>
     <div id="elev" class="elev">
      <img id="elevator" class="elevator" src="https://www.fujihd.com/wp-content/uploads/2018/12/HD-JX01.jpg"/>
     </div>
@@ -20,144 +20,72 @@ export default {
   components: { },
   data(){
     return{
-      lvl: 0,
-      el: ''
+      calls: []
     }
   },
 
   methods: {
 
-    move_Elevator5() {
-      //let el = document.getElementById('elevator').style.top= "-636px"
-      //.el.classList.add('move-up5')
-      // setTimeout(() =>{ //задержка в ~3 сек и отправляем лифт вниз
-      // this.get_Down5() }, 5000)
-      //this.lvl = 5
-      document.getElementById('elevator').style.top= "65px"
-      // document.getElementById('call5').disabled = true
-      // document.getElementById('call4').disabled = true
-      // document.getElementById('call3').disabled = true
-      // document.getElementById('call2').disabled = true
-      // document.getElementById('call1').disabled = true
-      setTimeout(function(){
-      document.getElementById('call4').disabled = false
-      document.getElementById('call3').disabled = false
-      document.getElementById('call2').disabled = false
-      document.getElementById('call1').disabled = false
-      },3000);
-      // setTimeout(() => 3000)
-      // document.getElementById('call4').disabled = 'false'
-      // document.getElementById('call3').disabled = 'false'
-      // document.getElementById('call2').disabled = 'false'
-      // document.getElementById('call1').disabled = 'false'
+    call_Elevator(lvl){
+      this.calls.push(lvl)
+      console.log(this.calls)
+        if(this.calls[0] == 1){
+          this.move_Elevator1()
+          this.calls.shift()
+          console.log(this.calls)
+        }else if(this.calls[0] == 2){
+          this.move_Elevator2()
+          this.calls.shift()
+          console.log(this.calls)
+        }else if(this.calls[0] == 3){
+          this.move_Elevator3()
+          this.calls.shift()
+          console.log(this.calls)
+        }else if(this.calls[0] == 4){
+          this.move_Elevator4()
+          this.calls.shift()
+          console.log(this.calls)
+        }else if(this.calls[0] == 5){
+          this.move_Elevator5()
+          this.calls.shift()
+          console.log(this.calls)
+        }
       },
-    // get_Down5(){
-    //   let el = document.getElementById('elevator')
-    //   el.classList.remove('move-up5') //двигаемся вниз, удаляя класс
-    //   this.lvl = 0
-    // }, 
+
+    move_Elevator5() {
+      document.getElementById('elevator').style.top= "65px"
+      document.getElementById('call5').disabled = true
+      setTimeout(3000)
+      document.getElementById('call5').disabled = false
+      },
 
     move_Elevator4() {
-      // this.lvl = 4
-      // let el = document.getElementById('elevator')
-      // el.classList.add('move-up4')
-      // setTimeout(() =>{ //задержка в ~3 сек и отправляем лифт вниз
-      // this.get_Down4() }, 5000)
       document.getElementById('elevator').style.top= "225px"
-      // document.getElementById('call5').disabled = true
-      // document.getElementById('call4').disabled = true
-      // document.getElementById('call3').disabled = true
-      // document.getElementById('call2').disabled = true
-      // document.getElementById('call1').disabled = true
-      setTimeout(function(){
+      document.getElementById('call4').disabled = true
+      setTimeout(() => 3000)
       document.getElementById('call5').disabled = false
-      document.getElementById('call3').disabled = false
-      document.getElementById('call2').disabled = false
-      document.getElementById('call1').disabled = false
-      },3000);
-      //  setTimeout(() => 3000)
-      // },
-    // get_Down4(){
-    //   let el = document.getElementById('elevator')
-    //   el.classList.remove('move-up4') //двигаемся вниз, удаляя класс
-    //   this.lvl = 0
     }, 
 
     move_Elevator3() {
-      // let el = document.getElementById('elevator')
-      // el.classList.add('move-up3')
-      // setTimeout(() =>{ //задержка в ~3 сек и отправляем лифт вниз
-      // this.get_Down3() }, 5000)
-       //this.lvl = 3
       document.getElementById('elevator').style.top= "380px"
-      // document.getElementById('call5').disabled = true
-      // document.getElementById('call4').disabled = true
-      // document.getElementById('call3').disabled = true
-      // document.getElementById('call2').disabled = true
-      // document.getElementById('call1').disabled = true
-      setTimeout(function(){
+      document.getElementById('call3').disabled = true
+      setTimeout(() => 3000)
       document.getElementById('call5').disabled = false
-      document.getElementById('call4').disabled = false
-      document.getElementById('call2').disabled = false
-      document.getElementById('call1').disabled = false
-      },3000);
-      // setTimeout(() => 3000)
-      // },
-    // get_Down3(){
-    //   let el = document.getElementById('elevator')
-    //   el.classList.remove('move-up3') //двигаемся вниз, удаляя класс
     }, 
 
     move_Elevator2() {
-      // let el = document.getElementById('elevator')
-      // el.classList.add('move-up2')
-      // // setTimeout(() =>{ //задержка в ~3 сек и отправляем лифт вниз
-      // // this.get_Down2() }, 5000)
-      //  this.lvl = 2
       document.getElementById('elevator').style.top= "540px"
-      // document.getElementById('call5').disabled = true
-      // document.getElementById('call4').disabled = true
-      // document.getElementById('call3').disabled = true
-      // document.getElementById('call2').disabled = true
-      // document.getElementById('call1').disabled = true
-      setTimeout(function(){
+      document.getElementById('call2').disabled = true
+      setTimeout(() => 3000)
       document.getElementById('call5').disabled = false
-      document.getElementById('call4').disabled = false
-      document.getElementById('call3').disabled = false
-      document.getElementById('call1').disabled = false
-      },3000);
-      // setTimeout(() => 3000)
-      // },
-    // get_Down2(){
-    //   let el = document.getElementById('elevator')
-    //   el.classList.remove('move-up2') //двигаемся вниз, удаляя класс
     }, 
 
     move_Elevator1() {
-      // let el = document.getElementById('elevator')
-      // el.classList.add('move-up1')
-      // setTimeout(() =>{ //задержка в ~3 сек и отправляем лифт вниз
-      // this.get_Down1() }, 5000)
-       //this.lvl = 1
       document.getElementById('elevator').style.top= "700px"
-      // document.getElementById('call5').disabled = true
-      // document.getElementById('call4').disabled = true
-      // document.getElementById('call3').disabled = true
-      // document.getElementById('call2').disabled = true
-      // document.getElementById('call1').disabled = true
-      setTimeout(function(){
-      document.getElementById('call5').disabled = false
-      document.getElementById('call4').disabled = false
-      document.getElementById('call3').disabled = false
-      document.getElementById('call2').disabled = false
-      },3000);
-      // setTimeout(() => 5000)
-      // },
-    // get_Down1(){
-    //   let el = document.getElementById('elevator')
-    //   el.classList.remove('move-up1') //двигаемся вниз, удаляя класс
+      document.getElementById('call1').disabled = true
+      setTimeout(() => 5000)
     },   
-  },
+  }
 }
 
 </script>
